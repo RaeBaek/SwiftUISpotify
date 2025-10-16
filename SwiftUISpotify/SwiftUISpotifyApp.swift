3b87111f577ec979d8124dddcf5e73f89cd4f6ea
RaeBaek <bmp117@naver.com>
2025-10-16 15:08:39 +0900

    Initial Commit

---
+++ SwiftUISpotify/SwiftUISpotifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISpotifyApp.swift
+//  SwiftUISpotify
+//
+//  Created by 백래훈 on 10/16/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISpotifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
